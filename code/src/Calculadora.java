03d1c63771ce395b0553227e3d169a900122cb58
PedroBelo1550 <50001735+PedroBelo1550@users.noreply.github.com>
2022-03-11 21:12:21 -0300

    implementação de testes de soma e divisão por zero

--- code/src/Calculadora.java
+++ code/src/Calculadora.java
@@ -2,4 +2,25 @@ import java.util.List;
 
 public class Calculadora {
     private List<Double> inputs;
+
+    public double soma(double a, double b){
+        return a + b;
+    }
+
+    public double subtracao(double a, double b){
+        return a - b;
+    }
+
+    public double multiplicacao(double a, double b){
+        return a * b;
+    }
+
+    public double divisao(double a, double b){
+        
+        if(b == 0)
+            throw new ArithmeticException("O denominador não pode ser zero!");
+        
+        return a / b;
+    }
+
 }
--- code/test/CalculadoraTest.java
+++ code/test/CalculadoraTest.java
@@ -1,6 +1,9 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.internal.runners.statements.ExpectException;
 
 public class CalculadoraTest {
 
@@ -9,4 +12,30 @@ public class CalculadoraTest {
         Calculadora calculadora = new Calculadora();
         System.out.println("Hello World Test");
     }
+
+    @Test
+    public void testeSoma() {
+        Calculadora calculadora = new Calculadora();
+
+        double esperado = 2;
+        double resultado = calculadora.soma(1, 1);
+
+        System.out.println("Teste Soma");
+        assertEquals(esperado, resultado, "Resultado de soma não esperado!");
+
+    }
+
+    @Test
+    public void testeDivisaoZero() {
+
+        Calculadora calculadora = new Calculadora();
+
+        System.out.println("Teste divisão por zero");
+        ArithmeticException arithmeticException = assertThrows(ArithmeticException.class,
+                () -> calculadora.divisao(1, 0));
+
+        assertTrue(arithmeticException.getMessage().contains("O denominador não pode ser zero!"));
+
+    }
+
 }
