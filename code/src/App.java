7f98b5ec1add0dacbcd06dd5c01529cfd817a9bd
PedroBelo1550 <50001735+PedroBelo1550@users.noreply.github.com>
2022-03-11 22:13:19 -0300

    correção de pasta

--- code/src/App.java
+++ code/src/App.java
@@ -3,7 +3,7 @@
 public class App {
     public static void main(String[] args) {
 	
-        System.out.println("Teste Calculadora");
+        System.out.println("Teste Calculadora ");
         
 
     }
